e2474c30e31d7b70c15b88816a75670addb73d1a
talitaester <talitaalves791@gmail.com>
2025-02-19 23:09:03 -0300

    mudando cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://atipicidades-ssks.vercel.app', 
     credentials: true 
 }));
 app.use(cookieParser());
